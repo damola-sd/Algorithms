65c99cdfa4532727cb317a1f8483a9a2681b6b6a
Damola Adewunmi <damolasd@gmail.com>
2019-10-10 16:44:34 +0100

    Used Sorin's solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,17 +3,27 @@
 import sys
 
 def rock_paper_scissors(n):
-  arr = []
-  moves = ['rock', 'paper', 'scissors']
-  if n <= 0:
-    return 0
-  if n >= 1:
-    for i in range(0, len(moves) - 1):
-      arr[i].append(moves[i])
+   # define a list with R-P-S options and
+    # list to contain the results
+    choice = ['rock', 'paper', 'scissors']
+    result = []
 
-    rock_paper_scissors(n-1)
-  return arr
+    # helper funciton to pass number of round 
+    # and a list that contains results from previous rounds
+    def helper(rounds, saved_arr):
+        # end condition
+        if rounds == 0:
+            # if we're at 0 rounds, append list to the resulting list
+            result.append(saved_arr)
+            return
 
+        # for every option in rps add the option to the list and run the fn again
+        for i in choice:
+            # run this function 3 times for every 'rock', 'paper', 'scissors' rounds number of times
+            new_arr = saved_arr + [i]
+            helper(rounds-1, new_arr)
+    helper(n, [])
+    return result
 
 
 if __name__ == "__main__":
